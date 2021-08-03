d602a23f371daeb4729d99abfb8b5eea935c69c5
z5401009702 <cdkk5555@qq.com>
2021-08-03 09:46:50 +0800

    updata

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
-import unittest
+import unittest,os
 from BeautifulReport import BeautifulReport
 
 if __name__ == '__main__':
+    localpath = os.getcwd()
+    filepath = os.path.join(localpath,'Report')
     test_suite = unittest.defaultTestLoader.discover('case', pattern='test*.py')
     result = BeautifulReport(test_suite)
-    result.report(filename='booking场景测试报告', description='booking场景自动化报告')
+    result.report(filename='booking场景测试报告', description='booking场景自动化报告',log_path=filepath)
 
